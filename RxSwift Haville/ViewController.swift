b6872ff9dd161bc2d526a55df697ffa7dccf23f0
Victor Idongesit <victoridongesit@gmail.com>
2018-06-05 16:21:49 +0100

    Initial Commit

---
+++ RxSwift Haville/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwift Haville
+//
+//  Created by Victor Idongesit on 05/06/2018.
+//  Copyright © 2018 Victor Idongesit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
